e9f71c61835d7258c5f7b81173eef823f9e6d4d4
Michal Zadora <michalzadora@localhost.localdomain>
2019-04-06 14:57:24 +0200

    Add mainView

--- src/main/java/pl/agh/edu/timekeeper/Main.java
+++ src/main/java/pl/agh/edu/timekeeper/Main.java
@@ -1,8 +1,10 @@
 package pl.agh.edu.timekeeper;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,9 +15,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello world");
-        Label label = new Label("Hello world");
-        Scene scene = new Scene(label, 300, 250);
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/views/mainView.fxml"));
+        BorderPane pane = new BorderPane(loader.load());
+        Scene scene = new Scene(pane);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
