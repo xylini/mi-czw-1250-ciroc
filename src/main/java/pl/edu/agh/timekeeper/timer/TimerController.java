1244bce43ed44c8bd51bc8f954ef1d9482dd4d0b
michalzadora <mz4900@gmail.com>
2019-06-11 16:25:12 +0200

    read TODO

--- src/main/java/pl/edu/agh/timekeeper/timer/TimerController.java
+++ src/main/java/pl/edu/agh/timekeeper/timer/TimerController.java
@@ -1,15 +1,24 @@
 package pl.edu.agh.timekeeper.timer;
 
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Screen;
+import javafx.stage.Stage;
 import pl.edu.agh.timekeeper.db.dao.ApplicationDao;
 import pl.edu.agh.timekeeper.db.dao.LogApplicationDao;
 import pl.edu.agh.timekeeper.log.LogApplication;
 import pl.edu.agh.timekeeper.model.Application;
+import pl.edu.agh.timekeeper.model.MyTime;
 import pl.edu.agh.timekeeper.model.Restriction;
+import pl.edu.agh.timekeeper.model.TimePair;
 import pl.edu.agh.timekeeper.windows.FocusedWindowManager;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 public class TimerController {
@@ -51,6 +60,17 @@ public class TimerController {
         updateTimerViewTimeWorker(this.timerView);
     }
 
+    private void closeApplication(String path) {
+        String command = "wmic process where ExecutablePath='" + path.replace("\\", "\\\\") + "' delete";
+        Process powerShellProcess = null;
+        try {
+            powerShellProcess = Runtime.getRuntime().exec(command);
+            powerShellProcess.getOutputStream().close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void mainLoop(TimerView timerView) {
         Thread t = new Thread(() -> {
             try {
@@ -78,6 +98,36 @@ public class TimerController {
                         currentWindowPath = fwm.getForegroundWindowPath();
                         currentRestrictedAppTillNow = setCurrApplicationUsageTimeIfRestricted(isCurrentWindowRestricted, currentWindowPath);
                         logIfPrevWindowRestricted(prevWindowPath, isPrevWindowRestricted, prevTimeStart, prevTimeStop);
+//TODO: Póki co mamy alert który ma przycisk close który ubija poprzednią apkę czyli ta na ktorej sie overtime zrobil
+//                       trzeba zrobic przycisk do override i w sumie jesli nie chce sie przedlozyc albo zostawic close
+//                        obrona przed zamknieciem (albo zrobic ze zamkniecie zamyka apke - to chyba drastyczne),
+//                        albo zablokowac go po prostu, nie ma logiki overrida tu ale chyba Krystian cos zrobil ?
+//                        nie jest skonczone sprawdzania hours range ale to w sumie mozna latwo zrobic
+                        if (isCurrentWindowRestricted) {
+                            Optional<Application> app = applicationDao.getByPath(currentWindowPath);
+                            long timeUsage = logApplicationDaoBase.getUsageInMillisOn(LocalDate.now(), app.get());
+                            if (getMillis(app.get().getRestriction().getLimit()) <= timeUsage ||
+                                    isNowBlocked(app.get(), app.get().getRestriction().getBlockedHours())) {
+                                Platform.runLater(() -> {
+                                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                                    alert.setHeaderText("Override");
+                                    alert.getDialogPane().setMinSize(800, 800);
+                                    alert.getDialogPane().toFront();
+                                    ButtonType closeApp = new ButtonType("Close");
+                                    alert.getButtonTypes().add(closeApp);
+                                    Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+                                    stage.setAlwaysOnTop(true);
+                                    Optional<ButtonType> option = alert.showAndWait();
+
+                                    if (option.get() == null) {
+                                    } else if (option.get() == closeApp) {
+                                        closeApplication(prevWindowPath);
+                                    }
+                                });
+                            }
+                        }
+
+
                     }
                     timeStop.setTime(System.currentTimeMillis());
                 }
@@ -89,6 +139,16 @@ public class TimerController {
         });
         t.setDaemon(true);
         t.start();
+    }
+
+    private boolean isNowBlocked(Application application, List<TimePair> blockedHours) {
+        int hour = LocalTime.now().getHour();
+        int minute = LocalTime.now().getMinute();
+        for (TimePair timePair : blockedHours) {
+            if (timePair.getStart().getHour() <= hour && hour <= timePair.getEnd().getHour())
+                return true;
+        }
+        return false;
     }
 
     private void updateTimerViewCoords(TimerView timerView) {
@@ -201,5 +261,11 @@ public class TimerController {
         return resultTime;
     }
 
+    private long getMillis(MyTime time) {
+        long millis = 0;
+        millis += time.getHour() * 60 * 60 * 1000;
+        millis += time.getMinute() * 60 * 1000;
+        return millis;
+    }
 }
 
